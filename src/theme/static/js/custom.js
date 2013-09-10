ae72546a74c7e65bce523089acaa0d0399d55007
Mateusz <matimilan@o2.pl>
2013-09-10 22:57:39 +0200

    filler

--- src/theme/static/js/custom.js
+++ src/theme/static/js/custom.js
@@ -4,6 +4,8 @@
  */
 jQuery.noConflict();
 jQuery(document).ready(function($) {
+    $('.simplebullets').css('width','none!important');
+    console.log($('.simplebullets').css());
 	// Home page 
     $('#home_responsive').isotope({
 		itemSelector: '.hp-wrapper',
